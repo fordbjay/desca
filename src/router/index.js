bb0a9ba40de541e9320e06835b21f1ce5c8c4e6e
Brian Ford <brianfordcode@gmail.com>
2024-09-18 18:33:54 -0500

    implement router

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    { 
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        meta: {
+            title: "Home"
+          }
+    },
+    { 
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue'),
+        meta: {
+            title: "About"
+          }
+    },
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+})
+
+export default router
