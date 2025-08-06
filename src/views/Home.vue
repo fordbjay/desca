091d22d4f2dace158065a2a1d4af229beee3e030
Brian Ford <brianfordcode@gmail.com>
2025-08-06 13:31:24 -0500

    home/signin page formatting left

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,26 @@
 <template>
 
-  <div style="display: flex; justify-content: space-around; align-items: center; height: 100vh">
+  <div class="main-container">
 
-    <div style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
+    <div class="action-wrapper">
 
-      <img src="/desca-logo.png" alt="logo" style="width: 250px;">
+
+      <div class="top-logo-motto">
+
+        <!-- LOGO -->
+        <img class="logo" src="/desca-logo.png" alt="logo" style="width: 250px;">
+
+        <!-- MOTTO -->
+        <h1>The #1 destination to share YOUR ultimate gaming, streaming or working setup</h1>
+      </div>  
+
       
-      <div style="display: flex; align-items: center;">
+      
+        <button @click="logIn()">log in</button>
 
-        <img src="/item-icons/accessory.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/chair.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/computer.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/desk.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/headset.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/keyboard.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/microphone.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/monitor.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/mouse.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/speaker.svg" alt="accessory" style="height: 25px; margin: 10px;">
-        <img src="/item-icons/webcam.svg" alt="accessory" style="height: 25px; margin: 10px;">
-
-      </div>
-
-      <button @click="logIn()">log in</button>
-    
     </div>
+
+
 
   </div>
 
@@ -48,5 +44,44 @@ export default {
 </script>
 
 <style scoped>
+
+.main-container {
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  /* border: 1px solid blue; */
+}
+
+.action-wrapper {
+  /* border: 1px solid; */
+  /* width: min-content; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.logo {
+  
+}
+
+h1 {
+  /* border: 1px solid; */
+  width: 450px;
+  font-weight: 400;
+}
+
+button {
+  padding: 0.75rem 2rem;
+  margin-top: 50px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+
 
 </style>
